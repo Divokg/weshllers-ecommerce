c25f7c9f0852bfc616914df0270a869fcf3b6181
Samuel Ndungu <ndungusam1@gmail.com>
2022-06-16 00:05:30 +0300

    cart logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import React from 'react'
-import { Layout } from '../components'
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout>
+    <StateContext>
+        <Layout>
+          <Toaster/>
       <Component {...pageProps} />
   </Layout>
+    </StateContext>
+  
   )
 }
 
